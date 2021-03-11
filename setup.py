9d8342ab14b50b459cc4057706359e06204c19dd
Ove Haugvaldstad <ovehaugv@outlook.com>
2021-03-11 11:04:17 +0100

    update build process

--- setup.py
+++ setup.py
@@ -2,19 +2,16 @@ import setuptools
 from numpy.distutils.core import Extension
 from setuptools import find_packages
 
-ext1 = Extension(name='fpcluster.clustering', 
-            sources=['src/fpcluster/fpcluster.pyf', 'src/fpcluster/clustering.f90','src/fpcluster/par_mod.f90'])
-ext2 = Extension(name='fpcluster.clustering_whole',
-            sources=['src/fpcluster/fpcluster_whole.pyf','src/fpcluster/clustering_whole.f90','src/fpcluster/par_mod.f90'])
+ext1 = Extension(name='fpcluster.trajectory_clustering', 
+            sources=['fpcluster/clustering/trajectory_clustering.pyf', 'fpcluster/clustering/trajectory_clustering.f90'])
 
 if __name__ =="__main__":
     from numpy.distutils.core import setup
     
     setup(name='fpcluster',
-    version="0.0.1",
+    version="1.0.0",
     description= 'Python wrapper of FLEXPART trajectory clustering subroutine',
     author ="Ove Haugvaldstad",
     author_email='ovehaugv@outlook.com',
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    ext_modules = [ext1,ext2])
+    packages=find_packages(),
+    ext_modules = [ext1])
